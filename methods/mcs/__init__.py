ab1b0798ed2125368beceeea0a33c6ae46730ea4
Luka van der Plas <l.p.vanderplas@uu.nl>
2021-01-18 14:01:08 +0100

    more style fixes

--- methods/mcs/__init__.py
+++ methods/mcs/__init__.py
@@ -8,13 +8,14 @@ BIN_DIR = os.path.join(OWN_DIR, "bin")
 
 def split(word: str):
     # run java script as subprocess
-    p = subprocess.Popen(["java", "-jar", "MCS.jar", "--SPLIT", 
+    p = subprocess.Popen(["java", "-jar", "MCS.jar", "--SPLIT",
                           "--TERM", word,
                           "--LEMMASET", "MCS_lemmaset.tsv",
                           "--headMOPs", "MCS_mopset.tsv",
                           "--modifierMOPs", "MCS_mopset.tsv",
                           ],
-        cwd = BIN_DIR, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                          cwd=BIN_DIR, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                        )
 
     out, err = p.communicate()
     total_output = out.decode().strip()
@@ -42,7 +43,6 @@ def prepare():
     if not os.path.exists(BIN_DIR):
         # should have been retrieved using ~/retrieve.py
         os.mkdir(BIN_DIR)
-        
         def copy_resource(filename):
             copyfile(os.path.join(OWN_DIR, "..", "..", "dependencies", filename), os.path.join(BIN_DIR, filename))
         copy_resource("MCS.jar")
