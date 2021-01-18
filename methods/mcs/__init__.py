e432886419cb24c45d0a21bbfd34eecb103f22a0
Luka van der Plas <l.p.vanderplas@uu.nl>
2021-01-18 14:13:05 +0100

    fix indents

--- methods/mcs/__init__.py
+++ methods/mcs/__init__.py
@@ -14,8 +14,7 @@ def split(word: str):
                           "--headMOPs", "MCS_mopset.tsv",
                           "--modifierMOPs", "MCS_mopset.tsv",
                           ],
-                          cwd=BIN_DIR, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-                        )
+                         cwd=BIN_DIR, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     out, err = p.communicate()
     total_output = out.decode().strip()
@@ -43,8 +42,10 @@ def prepare():
     if not os.path.exists(BIN_DIR):
         # should have been retrieved using ~/retrieve.py
         os.mkdir(BIN_DIR)
+
         def copy_resource(filename):
             copyfile(os.path.join(OWN_DIR, "..", "..", "dependencies", filename), os.path.join(BIN_DIR, filename))
+
         copy_resource("MCS.jar")
         copy_resource("MCS_lemmaset.tsv")
         copy_resource("MCS_mopset.tsv")
