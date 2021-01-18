1480ab789a207074c2fb9a2afa26afd271bad014
Luka van der Plas <l.p.vanderplas@uu.nl>
2021-01-18 13:42:08 +0100

    style fixes

--- retrieve.py
+++ retrieve.py
@@ -18,7 +18,7 @@ files = [{
     "url": "https://dhstatic.hum.uu.nl/compound-splitter/secos-nl.zip",
     "manual": False
 }, {
-    "filename": "MCS.jar", 
+    "filename": "MCS.jar",
     # original https://www.ims.uni-stuttgart.de/documents/ressourcen/werkzeuge/MCSfiles/MCS.jar
     "url": "https://dhstatic.hum.uu.nl/compound-splitter/MCS.jar",
     "manual": False
